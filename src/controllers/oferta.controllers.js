d7f05a818259090eaadab2b976c53deb479f4e90
opaulosaa <pauloarneto@gmail.com>
2026-03-05 21:35:27 -0300

    fix: ajustes finais de ofertas

--- src/controllers/oferta.controllers.js
+++ src/controllers/oferta.controllers.js
@@ -8,13 +8,21 @@ exports.createOfertaController = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
-
-<<<<<<< HEAD
-=======
 /**
  * Controller: atualiza uma oferta.
  */
->>>>>>> 0061769c8c28721ce0a6fb29a0cbdb3f68e9dc73
+exports.updateOfertaController = async (req, res) => {
+  try {   
+    const { id } = req.params;
+   
+    const oferta = await ofertaService.updateOferta(id, req.body);
+  
+    return res.status(200).json({ message: "Oferta atualizada com sucesso", oferta });
+  } catch (error) {
+   
+    return res.status(400).json({ error: error.message });
+  }
+};
 exports.updateOfertaController = async (req, res) => {
   try {   
     const { id } = req.params;
@@ -28,12 +36,10 @@ exports.updateOfertaController = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-=======
+
 /**
  * Controller: remove uma oferta pelo id.
  */
->>>>>>> 0061769c8c28721ce0a6fb29a0cbdb3f68e9dc73
 exports.deleteOfertaController = async (req, res) => {
   try {
     const { id } = req.params;
@@ -47,12 +53,10 @@ exports.deleteOfertaController = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-=======
+
 /**
  * Controller: lista ofertas com filtros.
  */
->>>>>>> 0061769c8c28721ce0a6fb29a0cbdb3f68e9dc73
 exports.listOfertasController = async (req, res) => {
   try {
    
